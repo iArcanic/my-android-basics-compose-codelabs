02dd662e4f8c9d0252611e3f326cd09a130c0a7a
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-14 14:47:13 +0100

    Added padding

--- first-android-app/app/src/main/java/com/preetham/greetingcard/MainActivity.kt
+++ first-android-app/app/src/main/java/com/preetham/greetingcard/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.preetham.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +34,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Red) {
         Text(
             text = "Hi, my name is $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }

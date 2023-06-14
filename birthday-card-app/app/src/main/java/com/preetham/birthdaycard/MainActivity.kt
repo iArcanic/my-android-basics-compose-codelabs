0914b84a0014b79a8a12a0c0ef9262422a042cbb
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-14 16:11:56 +0100

    Changed opacity and image scale

--- birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
+++ birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,7 +68,9 @@ fun BirthdayImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         BirthdayText(
             message = message,

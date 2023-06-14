990346b5faea34ed22b4148d439d90f04146dd15
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-14 16:24:46 +0100

    Extracted string resources

--- birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
+++ birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +34,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BirthdayImage(message = "Happy Birthday Preetham!", from = "From Android")
+                    BirthdayImage(
+                        message = getString(R.string.happy_birthday_text),
+                        from = getString(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -86,6 +90,9 @@ fun BirthdayImage(message: String, from: String, modifier: Modifier = Modifier) 
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCardTheme {
-        BirthdayImage(message = "Happy Birthday Preetham!", from = "From Android")
+        BirthdayImage(
+            message = stringResource(id = R.string.happy_birthday_text),
+            from = stringResource(id = R.string.signature_text)
+        )
     }
 }

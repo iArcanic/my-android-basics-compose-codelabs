712d336bb454f7c6dcb51ba83d11142aebcf03a5
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-14 15:39:37 +0100

    Added text alignment and padding

--- birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
+++ birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
@@ -3,14 +3,18 @@ package com.preetham.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.preetham.birthdaycard.ui.theme.BirthdayCardTheme
 
@@ -33,11 +37,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthdayText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
         Text(
             text = message,
             fontSize = 100.sp,
-            lineHeight = 116.sp
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,

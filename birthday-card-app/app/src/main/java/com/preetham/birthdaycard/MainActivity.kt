80d91b003d8b7105b03aed303713c5b8d98cad02
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-14 16:26:56 +0100

    Aligned all text elements to the center

--- birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
+++ birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
@@ -61,7 +61,7 @@ fun BirthdayText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }

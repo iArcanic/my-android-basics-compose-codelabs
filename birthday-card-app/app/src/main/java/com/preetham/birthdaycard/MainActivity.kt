141ad5d79e2741c98cb863fa2e70740a3418f9ed
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-14 16:00:37 +0100

    Added image composable

--- birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
+++ birthday-card-app/app/src/main/java/com/preetham/birthdaycard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.preetham.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,13 +62,18 @@ fun BirthdayText(message: String, from: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun BirthdayImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
 
+    Image(
+        painter = image,
+        contentDescription = null
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCardTheme {
-        BirthdayText(message = "Happy Birthday Preetham!", from = "From Android")
+        BirthdayImage(message = "Happy Birthday Preetham!", from = "From Android")
     }
 }
